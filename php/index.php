aee751e5390d1075be6ce9fbee56a9ddee6ad61b
Mert Eser Meral <eser.meral@ogr.sakarya.edu.tr>
2022-05-11 17:06:19 +0300

    rütuşlar

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Kullanıcı Girişi</title>
-    <link rel="stylesheet" type="text/css" href="style.css" >
+    
 
     <style type="text/css">
         .login-page {
